86451afc0cfdb7f3f2a80ee3e6bdd08922de7a19
58 <115112505+digits58@users.noreply.github.com>
2022-06-12 00:53:06 -0500

    Initialize repository

---
+++ .dir-locals.el
@@ -0,0 +1,18 @@
+;; configures include directories and the c++ standard library for linters
+((c++-mode . ((eval . (set (make-local-variable 'project-path)
+                           (locate-dominating-file default-directory ".dir-locals.el")))
+              (eval . (set (make-local-variable 'project-includes)
+                           (mapcar (lambda (p) (concat project-path p))
+                                   '("include"
+                                     "extern/imgui"
+                                     "extern/imgui/backends"
+                                     "extern/Catch2/src"
+                                     "extern/glfw/include"
+                                     "extern/stb"))))
+              (eval . (set (make-local-variable 'clang-args)
+                           (cons "-std=c++1z"
+                                 (mapcar (lambda (p) (concat "-I" p)) project-includes))))
+              (company-clang-arguments . clang-args)
+              (flycheck-clang-tidy-extra-options . "-extra-arg=-std=c++1z")
+              (flycheck-clang-language-standard . "c++1z")
+              (flycheck-clang-include-path . project-includes))))
---
+++ include/anitk/lib.h
@@ -0,0 +1,11 @@
+#include <filesystem>
+#include <set>
+#include <optional>
+
+#include <GL/gl.h>
+
+namespace fs = std::filesystem;
+
+std::optional<std::set<fs::path>> ListDirectory(const fs::path &p, bool sorted = false);
+std::optional<GLuint> LoadTextureFromFile(const char *filename, int *out_width, int *out_height);
+
